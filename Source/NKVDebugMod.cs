3d59d38ccaf596dfb33cbf0f96a04803b559b701
Никита Каргапольцев <kargnikita5@gmail.com>
2026-02-17 23:25:20 +0100

    Init commit

---
+++ Source/NKVDebugMod.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using NineSolsAPI;
+using NineSolsAPI.Utils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NKVDebugMod;
+
+[BepInDependency(NineSolsAPICore.PluginGUID)]
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class NKVDebugMod : BaseUnityPlugin {
+
+    private Harmony harmony = null!;
+
+    private void Awake() {
+        Log.Init(Logger);
+        RCGLifeCycle.DontDestroyForever(gameObject);
+        gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        harmony = Harmony.CreateAndPatchAll(typeof(NKVDebugMod).Assembly);
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void OnDestroy() {
+        harmony.UnpatchSelf();
+    }
+}
